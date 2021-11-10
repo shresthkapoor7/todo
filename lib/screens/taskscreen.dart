8bb36b9efd3cc9fe2ba993604093b09ba3c79fc3
Shresth Kapoor <shresthkapoor7@gmail.com>
2021-11-10 22:37:11 +0530

    Bottom Sheet and CheckBoxes

--- lib/screens/taskscreen.dart
+++ lib/screens/taskscreen.dart
@@ -1,6 +1,99 @@
 import 'package:flutter/material.dart';
 
+const kTextFieldDecoration = InputDecoration(
+  hintStyle: TextStyle(
+      fontSize: 15.0, color: Colors.grey, fontStyle: FontStyle.italic),
+  hintText: 'Enter a task to be added',
+  contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+  ),
+  enabledBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.red, width: 2.0),
+    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+  ),
+  focusedBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.yellow, width: 1.0),
+    borderRadius: BorderRadius.all(Radius.circular(0.0)),
+  ),
+);
+bool af = false;
+
 class TasksScreen extends StatelessWidget {
+  Widget buildBottomSheet(BuildContext context) {
+    return Container(
+        height: MediaQuery.of(context).size.height * 0.75,
+        decoration: BoxDecoration(
+            color: Colors.black,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20), topRight: Radius.circular(20))),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 8.0, right: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    icon: Icon(
+                      Icons.arrow_back_ios,
+                      color: Colors.red,
+                    ),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  Text(
+                    'Add Task',
+                    style: TextStyle(
+                        color: Colors.red,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        fontStyle: FontStyle.italic),
+                  ),
+                  Text(
+                    'At',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        fontStyle: FontStyle.italic),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 10.0, right: 10, top: 10, bottom: 10),
+              child: TextField(
+                keyboardType: TextInputType.text,
+                autofocus: true,
+                textAlign: TextAlign.center,
+                decoration: kTextFieldDecoration,
+              ),
+            ),
+            TextButton(
+                onPressed: () {
+                  af = true;
+                },
+                child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(10)),
+                    height: 50,
+                    width: double.infinity,
+                    child: Center(
+                        child: Text(
+                      'Add',
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ))))
+          ],
+        ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,7 +104,25 @@ class TasksScreen extends StatelessWidget {
           size: 30,
           color: Colors.white,
         ),
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet(
+            isScrollControlled: true,
+            context: context,
+            builder: buildBottomSheet,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+          );
+          // builder: (context) => SingleChildScrollView(
+          //         child: Container(
+          //       decoration: BoxDecoration(
+          //           borderRadius:
+          //               BorderRadius.only(topLeft: Radius.circular(20))),
+          //       padding: EdgeInsets.only(
+          //           bottom: MediaQuery.of(context).viewInsets.bottom),
+          //       child: buildBottomSheet(context),
+          //     )));
+        },
       ),
       backgroundColor: Colors.black,
       body: SafeArea(
@@ -19,7 +130,7 @@ class TasksScreen extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Padding(
-              padding: EdgeInsets.only(top: 40.0, left: 20),
+              padding: EdgeInsets.only(top: 20.0, left: 20),
               child: CircleAvatar(
                 backgroundColor: Colors.white,
                 radius: 25,
@@ -52,7 +163,7 @@ class TasksScreen extends StatelessWidget {
               ),
             ),
             SizedBox(
-              height: 25,
+              height: 20,
             ),
             Flexible(
               child: Container(
@@ -86,10 +197,17 @@ class TasksScreen extends StatelessWidget {
   }
 }
 
-class Item extends StatelessWidget {
+class Item extends StatefulWidget {
   String txt = 'item';
-  bool press = false;
+
   Item({required this.txt});
+
+  @override
+  _ItemState createState() => _ItemState();
+}
+
+class _ItemState extends State<Item> {
+  bool press = false;
 
   @override
   Widget build(BuildContext context) {
@@ -99,16 +217,22 @@ class Item extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Text(
-              txt,
-              style: TextStyle(color: Colors.black, fontSize: 20),
+              widget.txt,
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  decoration:
+                      press ? TextDecoration.none : TextDecoration.lineThrough),
             ),
             IconButton(
               onPressed: () {
-                if (press == true) {
-                  press = false;
-                } else {
-                  press = true;
-                }
+                setState(() {
+                  if (press == true) {
+                    press = false;
+                  } else {
+                    press = true;
+                  }
+                });
               },
               icon: press
                   ? Icon(Icons.check_box_outline_blank)
